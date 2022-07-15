47f68e2b59a467ecec2970062d84ffea1af4e81e
Gustavo Kreischer de Almeida <gustavokreischer@gmail.com>
2022-07-15 16:05:59 -0300

    Initianting about component

---
+++ src/components/Carousel.js
@@ -0,0 +1,51 @@
+import Carousel from 'react-bootstrap/Carousel';
+import image01 from './../assets/images/01.jpg';
+import image02 from './../assets/images/02.jpg';
+import image03 from './../assets/images/03.jpg';
+
+
+function CarouselComponent() {
+    return (
+        <Carousel fade>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image01}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image02}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image03}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
+export default CarouselComponent;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function Navbar() {
-    return(
-        <React.Fragment>
-            <h1>Teste</h1>
-        </React.Fragment>
+import logomarca from './../assets/images/Logo.jpg';
+
+function NavbarComponent() {
+
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="www.sigatecinformatica.com.br">
+                    <img
+                        src={logomarca}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Sobre</Nav.Link>
+                        <Nav.Link href="#action3">Serviços</Nav.Link>
+
+                    </Nav>
+                    {/* <Button variant="primary">Login</Button> */}
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
-export default Navbar;
+export default NavbarComponent;
